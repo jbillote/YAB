abcf2263ed84a82269023650cc63987e4fb2ad8d
jbillote <jbillote@users.noreply.github.com>
2023-02-01 17:03:37 -0800

    Implement MBTL API information
    Implement MBTL API response information into embeds

--- commands/mbtl_framedata.go
+++ commands/mbtl_framedata.go
@@ -1,27 +1,47 @@
 package commands
 
 import (
-    "fmt"
-    "github.com/bwmarrin/discordgo"
-    "github.com/jbillote/YAB/graphql"
-    "github.com/jbillote/YAB/util/logger"
+	"github.com/bwmarrin/discordgo"
+	"github.com/jbillote/YAB/graphql"
+	"github.com/jbillote/YAB/util"
+	"github.com/jbillote/YAB/util/logger"
 )
 
 func mbtlHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
-    log := logger.GetLogger("MBTL Frame Data")
+	log := logger.GetLogger("MBTL Frame Data")
 
-    log.Info("Fetching options")
-    options := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(i.ApplicationCommandData().Options))
-    for _, o := range i.ApplicationCommandData().Options {
-        options[o.Name] = o
-    }
+	log.Info("Fetching options")
+	options := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(i.ApplicationCommandData().Options))
+	for _, o := range i.ApplicationCommandData().Options {
+		options[o.Name] = o
+	}
 
-    move, _ := graphql.QueryMBTLMove(options["character"].StringValue(), options["input"].StringValue())
+	move, _ := graphql.QueryMBTLMove(options["character"].StringValue(), options["input"].StringValue())
 
-    s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-        Type: discordgo.InteractionResponseChannelMessageWithSource,
-        Data: &discordgo.InteractionResponseData{
-            Content: fmt.Sprintf("%v", move),
-        },
-    })
+	embed := util.NewEmbed()
+	embed.SetTitle(move.Name)
+	embed.AddField("Input", move.Input, true)
+	embed.AddBlankField(true)
+	embed.AddBlankField(true)
+	embed.AddField("Damage", move.Damage, true)
+	embed.AddField("Block", move.Block, true)
+	embed.AddField("Cancel", move.Cancel, true)
+	embed.AddField("Property", move.Property, true)
+	embed.AddField("Cost", move.Cost, true)
+	embed.AddField("Attribute", move.Attribute, true)
+	embed.AddField("Startup", move.Startup, true)
+	embed.AddField("Active", move.Active, true)
+	embed.AddField("Recovery", move.Recovery, true)
+	embed.AddField("Overall", move.Overall, true)
+	embed.AddField("Advantage", move.Advantage, true)
+	embed.AddField("Invuln", move.Invuln, true)
+
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Embeds: []*discordgo.MessageEmbed{
+				embed.MessageEmbed,
+			},
+		},
+	})
 }
